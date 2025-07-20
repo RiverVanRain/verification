6c11dc64ba4c4b14249d0c9b42a0ff49f7525f26
Nikolai Shcherbin <hello@wzm.me>
2025-07-20 13:12:48 +0300

    Updates for Elgg 6.2

--- views/default/page/layouts/elements/header.php
+++ views/default/page/layouts/elements/header.php
@@ -15,14 +15,15 @@ if (!isset($header)) {
     unset($vars['title']);
 
     if ($title) {
+        $title_head = elgg_view_title($title, ['class' => 'elgg-heading-main']);
+
         $entity = elgg_extract('entity', $vars);
+
         if ($entity instanceof \ElggUser && $entity->verified_user) {
-            $title = elgg_format_element('h2', ['class' => 'elgg-heading-main'], $title . elgg_format_element('span', [
+            $title_head = elgg_format_element('h1', ['class' => 'elgg-heading-main'], $title . elgg_format_element('span', [
                 'class' => 'verified-badge',
                 'title' => elgg_echo('verified:account'),
             ]));
-        } else {
-            $title = elgg_view_title($title, ['class' => 'elgg-heading-main']);
         }
     }
 
@@ -32,7 +33,7 @@ if (!isset($header)) {
 
     $buttons = elgg_view_menu('title', $menu_params);
 
-    $header = $title . $buttons;
+    $header = $title_head . $buttons;
 }
 
 if (empty($header)) {
